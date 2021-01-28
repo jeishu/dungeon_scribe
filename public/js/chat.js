f28b8f37ced6ce6dbac899dbc2243305b828180d
Joey Sisk <jjmsisk@gmail.com>
2021-01-27 21:27:51 -0600

    DO NOT MERGE

---
+++ public/js/chat.js
@@ -0,0 +1,54 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+socket.emit("joinRoom", { username, room });
+
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const msg = e.target.elements.msg.value;
+
+  socket.emit("chatMessage", msg);
+
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("messge");
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+  <p class="text">
+    ${message}
+  </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
+
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map((user) => `<li>${user.username}</li>`).join("")}
+  `;
+}
+
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
