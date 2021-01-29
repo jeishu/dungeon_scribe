9b5700c33f084780ac57bd5eb60831ccdfc80d1a
jeishu <jeremy.zhu@utexas.edu>
2021-01-29 16:58:41 -0600

    Small changes in character section

--- models/character.js
+++ models/character.js
@@ -1,29 +1,29 @@
 module.exports = function(sequelize, DataTypes) {
-    var Character = sequelize.define("Character", {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      }
+  var Character = sequelize.define("Character", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    }
+  });
+
+  // Relationships
+  Character.associate = function(models) {
+    Character.hasMany(models.Note, {
+      onDelete: "cascade"
     });
 
-     // Relationships
-    Character.associate = function(models) {
-        Character.hasMany(models.Note, {
-            onDelete: "cascade"
-        });
+    Character.hasMany(models.Favorite, {
+      onDelete: "cascade"
+    });
 
-        Character.hasMany(models.Favorite, {
-            onDelete: "cascade"
-        });
-
-        Character.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-
-    return Character;
+    Character.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
   };
-  
+
+  return Character;
+};
+
