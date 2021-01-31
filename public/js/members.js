03c5a50f8b2b7ac9ff873d16b6261e2475ac66e8
Darren Garcia <darrengarcia159@gmail.com>
2021-01-31 13:14:36 -0600

    slider animation effect for characters- for presentation

--- public/js/members.js
+++ public/js/members.js
@@ -1,5 +1,77 @@
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".btn");
+let currentSlide = 1;
+
+// how to change images manually
+var manualNav = function(manual){
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
+
+  slides[manual].classList.add("active");
+  slides[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
+
+// script for image to move automatically
+
+var repeat = function(activeClass){
+  let active = document.getElementsByClassName("active");
+  let i = 1;
+
+  var repeater = () => {
+    setTimeout(function(){
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove("active");
+      });
+
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
+
+      if(slides.length == i){
+        i = 0;
+      }
+      if(i >= slides.length){
+        return;
+      }
+      repeater();
+    }, 10000);
+  };
+  repeater();
+};
+
+repeat();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() { // figure out which user is logged in and update page
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
 });
+
+
